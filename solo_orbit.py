b7bfa892acec5aa972ce5e2b0f4092f18f603394
Jonas Sinjan <einsteinisbae@gmail.com>
2023-07-15 21:21:57 +0200

    Add files via upload

--- solo_orbit.py
+++ solo_orbit.py
@@ -1,3 +1,4 @@
+from typing import NoReturn
 import numpy as np
 import os
 import spiceypy as sp
@@ -9,6 +10,13 @@ import matplotlib.pyplot as plt
 import sys
 from subprocess import Popen
 import glob
+import matplotlib.animation as animation
+import matplotlib
+import pandas as pd
+import matplotlib.patches as mpatches
+
+# matplotlib.use('agg')
+#plt.rcParams['animation.ffmpeg_path'] = '/opt/local/tools/ffmpeg/4.3.1/bin/ffmpeg'
 
 def loadkernel(kpath, kname):
     "This function loads a SPICE kernel (which could be a metakernel) then returns to the current working directory."
@@ -73,7 +81,7 @@ def find_nearest(array, value):
     idx = (np.abs(array - value)).argmin()
     return idx
 
-def main(time, plot = False):
+def main(time, plot = False, plot_movie=False, movie_start = None, movie_title = None):
     AU = 149598000.0 #in km
 
     #perform git pull on the kernels
@@ -100,6 +108,8 @@ def main(time, plot = False):
 
     ets = np.linspace(et_bounds[0],et_bounds[1],100000)
     ets[len(ets)-1]=et_bounds[1]
+
+    global earth_HCI_pos, venus_HCI_pos, solo_HCI_pos
 
     [solo_GSE_pos, ltime_gse] = sp.spkpos("SOLO",ets,"SOLO_GSE","NONE","EARTH")
     [solo_HCI_state, ltime] = sp.spkezr("SOLO",ets,"SOLO_HCI","NONE","SUN") #sp.spkezr("SOLO",ets,"SUN_INERTIAL","NONE","SUN")
@@ -154,6 +164,8 @@ def main(time, plot = False):
         print(f"Your desired time is {time} \n")
 
         desired_et = datetime642et(time)
+        
+        global nearest_idx_et
 
         nearest_idx_et = find_nearest(ets, desired_et)
         
@@ -218,7 +230,7 @@ def main(time, plot = False):
 
             solo_out_earth_line = 180-abs(solo_hlon_inst) + 180-abs(earth_hlon_inst)
 
-        print(f"Solo latitude out of Earth-Sun Line is {solo_out_earth_line:.4g} degrees \n")
+        print(f"Solo longitude out of Earth-Sun Line is {solo_out_earth_line:.4g} degrees \n")
 
         print(f"solo_hlon = {solo_hlon_inst:.4g} degrees ")
         print(f"earth_hlon = {earth_hlon_inst:.4g} degrees\n")
@@ -271,8 +283,8 @@ def main(time, plot = False):
         plt.savefig("./plots/inst_position_3d")
 
         points = 100
-        earth_orbit_traj = earth_HCI_pos[nearest_idx_et-points*10: nearest_idx_et+5*points].T
-        venus_orbit_traj = venus_HCI_pos[nearest_idx_et-points*10: nearest_idx_et+5*points].T
+        earth_orbit_traj = earth_HCI_pos[: nearest_idx_et+5*points].T #nearest_idx_et-points*10
+        venus_orbit_traj = venus_HCI_pos[: nearest_idx_et+5*points].T
 
         solo_orbit_traj = solo_HCI_pos[:nearest_idx_et+points].T #nearest_idx_et-points*10
 
@@ -287,6 +299,8 @@ def main(time, plot = False):
         #ax.add_patch(earth_traj) earth's trajectory not perfect circle - interested in the difference
         ax.plot(solo_orbit_traj[0],solo_orbit_traj[1], linestyle = '--', color = "blue")
         ax.scatter(0, 0, label = "SUN", color = "yellow", s = 100, edgecolors='black')
+        plt.ylim(-1.1,1.1)
+        plt.xlim(-1.1,1.1)
         plt.legend()
         plt.title(f'Solo HCI Position at {time} on Ecliptical plane')
         ax.set_xlabel("X (AU)")
@@ -294,10 +308,162 @@ def main(time, plot = False):
         
         #ax.view_init(elev = 90, azim = 90)
 
-        plt.savefig("./plots/inst_position_2d")
+        plt.savefig(f"./plots/inst_position_2d_{time}")
+
+    if plot_movie:
+
+        def make_movie(start=0, title = None):
+
+            if start == 0:
+                start_idx_et = 0
+            else:
+
+                des_et = datetime642et(start)
+
+                start_idx_et = find_nearest(ets, des_et)
+
+            global count
+            count = 0
+
+            def update(nearest_idx_et, count, init=start_idx_et):
+                
+                nearest_idx_et = int(nearest_idx_et)-1
+
+                inst_pos_hci = solo_HCI_pos[nearest_idx_et].T
+                inst_earth_hci = earth_HCI_pos[nearest_idx_et].T
+                inst_venus_hci = venus_HCI_pos[nearest_idx_et].T
+
+                scat_update = np.zeros((3,2))
+
+                scat_update[0,0] = inst_pos_hci[0]
+                scat_update[1,0] = inst_earth_hci[0]
+                scat_update[2,0] = inst_venus_hci[0]
+
+                scat_update[0,1] = inst_pos_hci[1]
+                scat_update[1,1] = inst_earth_hci[1]
+                scat_update[2,1] = inst_venus_hci[1]
+
+                scat.set_offsets(scat_update[0,:])
+                scat_1.set_offsets(scat_update[1,:])
+                scat_2.set_offsets(scat_update[2,:])
+
+                scat.set_color('blue')
+                scat_1.set_color('green')
+                scat_2.set_color('orange')
+
+                scat.set_label('SOLO')
+                scat_1.set_label('EARTH')
+                scat_2.set_label('VENUS')
+
+                # plot = np.stack((solo_orbit_traj, earth_orbit_traj, venus_orbit_traj))
+                # print(plot.shape)
+
+                solo_orbit_traj_tmp = solo_HCI_pos[init:nearest_idx_et].T
+
+                date_ets = ets[nearest_idx_et]
+                date = et2datetime64(date_ets)
+
+                line.set_data(np.array(solo_orbit_traj_tmp[0]).astype(np.float32),np.array(solo_orbit_traj_tmp[1]).astype(np.float32))
+
+                #
+                dt = pd.to_datetime(date[0])
+                #print(dt.date())
+                text.set_text(f'{dt.date()}')
+
+                # if dt.date() == pd.Timestamp("2020-04-20T00").date():
+                #     april = mpatches.Rectangle((scat_update[0,0]-0.05, scat_update[0,1]-0.05), 0.1, 0.1, facecolor = 'red', edgecolor='blue', label = "Commissioning", zorder=3)
+                #     ax.add_patch(april)
+
+                # if dt.date() == pd.Timestamp("2020-11-17T00").date():
+                #     nov = mpatches.Rectangle((scat_update[0,0]-0.05, scat_update[0,1]-0.05), 0.1, 0.1, facecolor = 'black', edgecolor='blue',label = "Nov 2020", zorder=3)
+                #     ax.add_patch(nov)
+
+                # if dt.date() == pd.Timestamp("2021-02-23T00").date():
+                #     feb = mpatches.Rectangle((scat_update[0,0]-0.05, scat_update[0,1]-0.05), 0.1,0.1, facecolor = 'purple', edgecolor='blue', label = "Feb 2021", zorder=3)
+                #     ax.add_patch(feb)
+
+                
+                if dt.date() == pd.Timestamp("2022-03-06T00").date():
+                #abs(earth_hlon[nearest_idx_et]-solo_hlon[nearest_idx_et]) < 0.6 and inst_pos_hci[1] > 0 and dt.date() >= pd.Timestamp("2022-02-20T00").date():
+                    if count < 1:
+                        ax.plot(np.linspace(0,inst_earth_hci[0], 40), np.linspace(0, inst_earth_hci[1], 40), linestyle = ':', linewidth=2, color="black")
+                        count += 1
+
+                if dt.date() == pd.Timestamp("2022-03-26T00").date():
+                    peri = mpatches.Circle((scat_update[0,0],scat_update[0,1]), 0.05, facecolor=None, label = "Perihelion 0.3AU", edgecolor = "black")
+                    ax.add_patch(peri)
+                
+                ax.legend(loc="upper right")
+            
+                return scat, scat_1, scat_2, line, text,
+
+            fig, ax = plt.subplots(figsize=(9, 9))
+            ax.set_ylim(-1.1,1.1)
+            ax.set_xlim(-1.1,1.1)
+            ax.set_title('Solo HCI Position at on Ecliptical Plane')
+            
+            text = ax.text(0.5,0.10, "", size = 18, bbox={'facecolor':'w', 'alpha':0.5, 'pad':5,},
+                    transform=ax.transAxes, ha="center")
+            ax.set_xlabel("X (AU)")
+            ax.set_ylabel("Y (AU)")
+
+            ax.add_patch(mpatches.Circle((0,0), 0.1, linestyle = 'dashed', color = "gray", fill = None, alpha = 0.5))
+            ax.add_patch(mpatches.Circle((0,0), 0.2, linestyle = 'dashed', color = "gray", fill = None, alpha = 0.5))
+            ax.add_patch(mpatches.Circle((0,0), 0.3, linestyle = 'dashed', color = "gray", fill = None, alpha = 0.5))
+            ax.add_patch(mpatches.Circle((0,0), 0.4, linestyle = 'dashed', color = "gray", fill = None, alpha = 0.5))
+            ax.add_patch(mpatches.Circle((0,0), 0.5, linestyle = 'dashed', color = "gray", fill = None, alpha = 0.5))
+            ax.add_patch(mpatches.Circle((0,0), 0.6, linestyle = 'dashed', color = "gray", fill = None, alpha = 0.5))
+            #ax.add_patch(mpatches.Circle((0,0), 0.7, linestyle = 'dashed', color = "gray", fill = None, alpha = 0.5))
+            ax.add_patch(mpatches.Circle((0,0), 0.8, linestyle = 'dashed', color = "gray", fill = None, alpha = 0.5))
+            ax.add_patch(mpatches.Circle((0,0), 0.9, linestyle = 'dashed', color = "gray", fill = None, alpha = 0.5))
+
+            ax.text(0.06, -0.02, "0.1", size = 10)
+            ax.text(0.16, -0.02, "0.2", size = 10)
+            ax.text(0.26, -0.02, "0.3", size = 10)
+            ax.text(0.36, -0.02, "0.4", size = 10)
+            ax.text(0.46, -0.02, "0.5", size = 10)
+            ax.text(0.56, -0.02, "0.6", size = 10)
+            #ax.text(0.66, -0.04, "0.7", size = 10)
+            ax.text(0.76, -0.02, "0.8", size = 10)
+            ax.text(0.86, -0.02, "0.9", size = 10)
+            
+
+            ax.scatter(0, 0, label = "SUN", color = "yellow", s = 300, edgecolors='black')
 
 
-    #plt.
+            earth_orbit_traj = earth_HCI_pos[:nearest_idx_et:100].T #nearest_idx_et-points*10
+            venus_orbit_traj = venus_HCI_pos[:nearest_idx_et:100].T
+            #solo_orbit_traj = solo_HCI_pos[::100].T #nearest_idx_et-points*10
+            
+            ta = ax.plot(earth_orbit_traj[0],earth_orbit_traj[1], linestyle = '-', color='green') #green ##004D40
+            ta_2 = ax.plot(venus_orbit_traj[0],venus_orbit_traj[1], linestyle = '-', color='orange')
+            
+            scat = ax.scatter([],[], s=60, edgecolors='black')
+            scat_1 = ax.scatter([],[], s=60, edgecolors='black')
+            scat_2 = ax.scatter([],[], s=60, edgecolors='black')
+            #scat.set_array(np.array(['blue', 'orange', 'green']))
+
+            line = plt.plot([],[], linestyle = '-', color = "blue") #blue
+
+            line = line[0]
+
+            def init_plot():
+                scat.set_offsets([])
+                scat_1.set_offsets([])
+                scat_2.set_offsets([])
+                text.set_text('')
+                line.set_data([],[])
+                return scat, scat_1, scat_2, line, text,
+
+
+            ani = animation.FuncAnimation(fig, update, init_func = init_plot, fargs = (count, start_idx_et), frames = np.linspace(start_idx_et,len(ets[:nearest_idx_et]),300), interval = 50, blit=True, repeat=False)
+                            
+            ani.save(f'{title}.mp4', writer = 'ffmpeg')#, extra_args=['-vcodec', 'libx264'])
+
+            #plt.show()
+
+        make_movie(start=movie_start, title = movie_title)
+
 
 if len(sys.argv) > 1:
 
@@ -311,10 +477,34 @@ if len(sys.argv) > 1:
 
         plot_bool = False
 
+        plot_movie = False
+
+        start_time = None
+
+        mov_title = None
+
 else:
 
-    input_time = np.datetime64('2020-05-28T17:00')
+    input_time = np.datetime64('2023-10-06T00:00')
 
     plot_bool = False
 
-main(time = input_time, plot = plot_bool)
+    plot_movie = False#rue
+
+    start_time = np.datetime64('2020-02-20T17:00')
+
+    mov_title = "flight_till_now"
+
+for i in range(18,22):
+
+    input_time = np.datetime64(f'2023-10-{i}T00:00')
+
+    plot_bool = False
+
+    plot_movie = False#rue
+
+    start_time = np.datetime64('2020-02-20T17:00')
+
+    mov_title = "flight_till_now"
+
+    main(time = input_time, plot = plot_bool, plot_movie = plot_movie, movie_start = start_time, movie_title = mov_title)
